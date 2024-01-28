5edb284229432a6d9d70fe6da5a8ffef148795c2
esenckewitch <106761896+esenckewitch@users.noreply.github.com>
2024-01-28 09:14:11 +0500

    better victory window

--- src/prefabs/VictoryWindow.js
+++ src/prefabs/VictoryWindow.js
@@ -8,30 +8,33 @@ class VictoryWindow extends Phaser.GameObjects.Container {
 	constructor(scene, x, y) {
 		super(scene, x ?? 375, y ?? 128);
 
-		// victory_window
-		const victory_window = scene.add.image(0, 159, "victory_window");
-		this.add(victory_window);
-
 		// button
 		const button = new Button(scene, 188, -6);
 		this.add(button);
 
-		this.victory_window = victory_window;
+		// light
+		const light = scene.add.spine(-8, 144, "light", "light-atlas", new spine.SkinsAndAnimationBoundsProvider("animation", ["default"]));
+		light.skeleton.setSkinByName("default");
+		this.add(light);
+
+		// victory_window
+		const victory_window = scene.add.image(-4, 190, "victory_window");
+		this.add(victory_window);
+
 		this.button = button;
+		this.victory_window = victory_window;
 
 		/* START-USER-CTR-CODE */
-		// Write your code here.
+		light.animationState.setAnimation(0, "animation", true);
 		/* END-USER-CTR-CODE */
 	}
 
-	/** @type {Phaser.GameObjects.Image} */
-	victory_window;
 	/** @type {Button} */
 	button;
+	/** @type {Phaser.GameObjects.Image} */
+	victory_window;
 
 	/* START-USER-CODE */
-
-	// Write your code here.
 
 	/* END-USER-CODE */
 }
